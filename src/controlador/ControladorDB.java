60750e7c6776568006e3f5ae0dade694bfac465e
VladyslavaTrubina-ll <vladyslava.trubina@elorrieta-errekamari.com>
2026-05-04 16:44:12 +0200

    empieso trabajar con Controlador

---
+++ src/controlador/ControladorDB.java
@@ -0,0 +1,8 @@
+package controlador;
+
+public class ControladorDB {
+
+    public static void main(String[] args) {
+        System.out.println("ControladorDB funcionando correctamente.\nLOL DA");
+    }
+}
